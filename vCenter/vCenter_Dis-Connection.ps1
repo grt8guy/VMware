6a04023a6dcd3bc2f845272e5c0705b4ddd0d4b1
John W. Braunsdorf <grt8guy@yahoo.com>
2024-12-02 08:21:07 -0800

    Add files via upload

---
+++ vCenter/vCenter_Dis-Connection.ps1
@@ -0,0 +1,19 @@
+﻿<#
+.SYNOPSIS
+        Use to disconnect all VI-Connections to vCen
+
+.REQUIREMENTS
+
+        Powercli
+
+.CREATOR
+    John Braunsdorf
+
+.DATE
+    03/15/2020
+
+#>
+
+### Disconnect ALL VI connections
+Disconnect-VIServer $global:DefaultVIServers -Force -Confirm:$false
+Write-Host "Disconnected All Shell Connections $global:DefaultVIServers" -BackgroundColor Gray -ForegroundColor Green
